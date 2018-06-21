33393e42f672662bd69ff0fb19e145699af5f7b3
tikhomirov_d <dimasta00@gmail.com>
2018-06-21 21:30:36 +0300

    Prog is working now

--- modul 1/spacename/emuspace.py
+++ modul 1/spacename/emuspace.py
@@ -1,12 +1,52 @@
+def add(scopes, cur, what):
+    if cur not in scopes:
+        scopes[cur] = {}
+        scopes[cur]['vars'] = []
+        scopes[cur]['vars'].append(what)
+    else:
+        if 'vars' not in scopes[cur]:
+            scopes[cur]['vars'] = []
+            scopes[cur]['vars'].append(what)
+        else:
+            scopes[cur]['vars'].append(what)
+
+
+def create(scopes, cur, par):
+    if cur not in scopes:
+        scopes[cur] = {}
+        scopes[cur]['funcs'] = []
+        scopes[cur]['vars'] = []
+        scopes[par]['funcs'].append(cur)
+        scopes[cur]['parent'] = par
+    else:
+        if 'funcs' not in scopes[cur]:
+            scopes[cur]['funcs'] = []
+            scopes[cur]['parent'] = par
+            scopes[par]['funcs'].append(cur)
+        else:
+            scopes[cur]['funcs'].append(cur)
+            scopes[par]['funcs'].append(cur)
+
+
+def search(scopes, namespace, what):
+    if what in scopes[namespace]['vars']:
+        return namespace
+    else:
+        try:
+            upper_namespace = scopes[namespace]['parent']
+        except KeyError:
+            return None
+        return search(scopes, upper_namespace, what)
+
+
 n = int(input())
-dict = {}
+scopes = {'global': {'funcs': [], 'vars': []}}
 
 for i in range(n):
-    s = input().split()
-    if s[0] == 'add':
-        dict[s[2]] = s[1]
-    if s[0] == 'get':
-        if s[2] in dict.keys():
-            print(dict[s[2]])
-        else:
-            print("None")
+    command = input().split()
+    if command[0] == 'add':
+        add(scopes, command[1], command[2])
+    elif command[0] == 'create':
+        create(scopes, command[1], command[2])
+    elif command[0] == 'get':
+        print(search(scopes, command[1], command[2]))
