7a13b295963b60c7a9f1edf6a6cc0f65860a3385
Dmitry Tikhomirov <dimasta00@gmail.com>
2018-07-21 13:56:50 +0300

    Add Solution

---
+++ modul 1/1.6 Class inheritance/LoggableList.py
@@ -0,0 +1,12 @@
+import time
+
+
+class Loggable:
+    def log(self, msg):
+        print(str(time.ctime()) + ": " + str(msg))
+
+
+class LoggableList(list, Loggable):
+    def append(self, arg):
+        self.log(arg)
+        list.append(self, arg)
