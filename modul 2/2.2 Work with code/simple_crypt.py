28d1dff611353834677a76e922ec059a6f765379
Dmitry Tikhomirov <dimasta00@gmail.com>
2018-07-24 23:48:01 +0300

    Add solution

--- modul 2/2.2 Work with code/simple_crypt.py
+++ modul 2/2.2 Work with code/simple_crypt.py
@@ -1,1 +1,12 @@
+import simplecrypt
 
+with open("encrypted.bin", "rb") as inp:
+    encrypted = inp.read()
+dec = ""
+with open("passwords.txt", "r") as pas:
+    for line in pas:
+        try:
+            dec = simplecrypt.decrypt(line.strip(), encrypted)
+            print(dec)
+        except:
+            print("Except ")
